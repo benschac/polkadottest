b48550d1079559c62dd9a91175dba9cf860b2945
benjamin <benschac@gmail.com>
2023-01-13 09:24:10 -0600

    initial commit

--- App.js
+++ App.js
@@ -1,7 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { u8aToHex } from "@polkadot/util";
+import { bip39Generate, bip39ToSeed, waitReady } from "@polkadot/wasm-crypto";
 export default function App() {
+  // first wait until the WASM has been loaded (async init)
+  React.useEffect(() => {
+    (async () => {
+      await waitReady();
+      // generate phrase
+      const phrase = bip39Generate(12);
+
+      // get ed25519 seed from phrase
+      const seed = bip39ToSeed(phrase, "");
+
+      // display
+      console.log("phrase:", phrase);
+      console.log("seed:", u8aToHex(seed));
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
